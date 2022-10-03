e461c4b1a42bc4ceb44cbb7e63d7ab8dfd0bba26
heven <112459745+heven07@users.noreply.github.com>
2022-10-03 11:53:11 +0530

    Updated recon
    Updated recon with proper commenting and removed redundant commented code.

--- Modules/user_recon.py
+++ Modules/user_recon.py
@@ -32,10 +32,10 @@ def user():
 
 def chkfb(username):
     link1 = "https://facebook.com/" + username
-    resp1 = requests.get(link1,headers={'User-Agent': 'APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)'})
+    resp1 = requests.get(link1,headers={'User-Agent': 'APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)'}) #crawler bot user agent to get required resp
     status = BeautifulSoup(resp1.text, 'html.parser')
     if resp1.status_code == 200:
-        if status.find_all('html', {'class':'no_js','id':'facebook'}):
+        if status.find_all('html', {'class':'no_js','id':'facebook'}): #respcode still works but has multiple false positives. this bit fixes those false positives
             return False
         return True
     else:
@@ -43,14 +43,14 @@ def chkfb(username):
 
 def chkreddit(username):
     link2 = "https://www.reddit.com/user/" + username
-    resp2 = requests.get(link2,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'})
+    resp2 = requests.get(link2,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}) #appended user agent since it doesn't work too well with crawler bots
     status = BeautifulSoup(resp2.content, 'html.parser')
     for item in status.find_all('img'):
-        if item['src'] == "https://www.redditstatic.com/desktop2x/img/snoomoji/snoo_thoughtful.png":
+        if item['src'] == "https://www.redditstatic.com/desktop2x/img/snoomoji/snoo_thoughtful.png": #checks for certain tags that are specific to failure.
             return False
     return True
 
-def chkgit(username):
+def chkgit(username):                                               #unchanged since it still works
     link3 = "https://github.com/" + username
     resp3 = requests.get(link3)
     status = BeautifulSoup(resp3.text, 'html.parser')
@@ -59,20 +59,19 @@ def chkgit(username):
     else:
         return False 
 
-def chktwitter(username):
+def chktwitter(username):                                            #updated for proper working
     link4 = "https://twitter.com/" + username
-    resp4 = requests.get(link4,headers={'User-Agent': 'APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)'})
+    resp4 = requests.get(link4,headers={'User-Agent': 'APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)'}) #crawler bot user agent to get required resp
     status = BeautifulSoup(resp4.text, 'html.parser')
-    if status.find_all('html',{'class':"dog"}):
+    if status.find_all('html',{'class':"dog"}): #checks for certain tags that are specific to failure.
         return False
     return True
     
 def chkig(username):
     link5 = "https://instagram.com/" + username
-    resp5 = requests.get(link5,headers={'User-Agent': 'APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)'})
+    resp5 = requests.get(link5,headers={'User-Agent': 'APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)'}) #crawler bot user agent to get required resp
     status = BeautifulSoup(resp5.content, 'html.parser')
-    # print(status)
-    if status.find_all('link', {'href':'https://www.instagram.com/%s/'%username}):
+    if status.find_all('link', {'href':'https://www.instagram.com/%s/'%username}): #checks for certain tags that are specific to success.
         return True
     return False
 #user()
